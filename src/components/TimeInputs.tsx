ac55905f3aa30af753a97600ccf1e4a1f9720b32
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-11-08 08:41:10 +0900

    ✨ feat :: TimeInputs 컴포넌트 생성

---
+++ src/components/TimeInputs.tsx
@@ -0,0 +1,53 @@
+import { FormControl, FormLabel, HStack, Input, Tooltip } from '@chakra-ui/react';
+import React from 'react';
+
+import { getTimeErrorMessage } from '../utils/timeValidation';
+
+interface TimeInputsProps {
+  startTime: string;
+  endTime: string;
+  onStartTimeChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onEndTimeChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  startTimeError: string | null;
+  endTimeError: string | null;
+}
+
+function TimeInputs({
+  startTime,
+  endTime,
+  onStartTimeChange,
+  onEndTimeChange,
+  startTimeError,
+  endTimeError,
+}: TimeInputsProps) {
+  return (
+    <HStack width="100%">
+      <FormControl>
+        <FormLabel>시작 시간</FormLabel>
+        <Tooltip label={startTimeError} isOpen={!!startTimeError} placement="top">
+          <Input
+            type="time"
+            value={startTime}
+            onChange={onStartTimeChange}
+            onBlur={() => getTimeErrorMessage(startTime, endTime)}
+            isInvalid={!!startTimeError}
+          />
+        </Tooltip>
+      </FormControl>
+      <FormControl>
+        <FormLabel>종료 시간</FormLabel>
+        <Tooltip label={endTimeError} isOpen={!!endTimeError} placement="top">
+          <Input
+            type="time"
+            value={endTime}
+            onChange={onEndTimeChange}
+            onBlur={() => getTimeErrorMessage(startTime, endTime)}
+            isInvalid={!!endTimeError}
+          />
+        </Tooltip>
+      </FormControl>
+    </HStack>
+  );
+}
+
+export default TimeInputs;
