f7f651d1281587f58e9a9f94c391bc0266ed6313
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-11-08 08:38:51 +0900

    ✨ feat :: FormInput 컴포넌트 추가

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,18 @@
+import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import React from 'react';
+
+interface FormInputProps {
+  label: string;
+  type?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const FormInput: React.FC<FormInputProps> = ({ label, type = 'text', value, onChange }) => (
+  <FormControl>
+    <FormLabel>{label}</FormLabel>
+    <Input type={type} value={value} onChange={onChange} />
+  </FormControl>
+);
+
+export default FormInput;
