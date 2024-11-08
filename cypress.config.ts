8a7b20bd269f9d1d41264256d408a5cf55736c83
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-11-08 18:22:07 +0900

    👷 feat :: cypress CICD 파일 작성

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { defineConfig } from 'cypress';
+import mochawesome from 'cypress-mochawesome-reporter/plugin';
 
 export default defineConfig({
   component: {
@@ -11,9 +12,19 @@ export default defineConfig({
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // 노드 이벤트 설정
+      // mochawesome 리포터 설정
+      mochawesome(on);
+
+      // 기타 노드 이벤트 설정
     },
     baseUrl: 'http://localhost:5173', // 예시 URL
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+    },
     // 기타 설정 옵션
   },
 });
