0be55688ee8d11166759c478d6ed1aad1c32258d
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-11-08 18:09:00 +0900

    ✨ feat :: cypress가 프로젝트 내부 파일들을 인식할 수 있도록 기본 세팅

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  component: {
+    supportFile: 'cypress/support/component.ts',
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // 노드 이벤트 설정
+    },
+    baseUrl: 'http://localhost:5173', // 예시 URL
+    // 기타 설정 옵션
+  },
+});
